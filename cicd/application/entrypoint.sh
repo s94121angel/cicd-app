2b61664c13615b4749a25f0a288b111aeb14b43b
s94121angel <s94121angel@yahoo.com.tw>
2022-05-27 18:33:30 +0800

    first commit

---
+++ cicd/application/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PREFIX="${PREFIX:-default}"
+COMMITHASH="${COMMITHASH:- }"
+COMMITLOG="${COMMITLOG:- }"
+IPS=$(hostname -I)
+
+cat <<EOF > /var/www/html/index.html
+Hostname is ${PREFIX}_${HOSTNAME}
+Latest Commit Hash is ${PREFIX}_${COMMITHASH}
+Latest Commit Log is ${PREFIX}_${COMMITLOG}
+IP address list are ${PREFIX}_${IPS}
+Datetime is $(date)
+Angel is testing docker from localhost to control VM.
+EOF
+
+#Generate a new index.html under /var/www/html
+exec "$(command -v nginx)" -c /etc/nginx/nginx.conf -g "daemon off;"
